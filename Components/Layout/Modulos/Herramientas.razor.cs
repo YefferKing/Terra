07a7c4e1dfec9121e883fadaed3c8d488cb25113
Yefferson <yeffersonpeinado@gmail.com>
2024-11-24 18:15:32 -0500

    add modulo herramientas en la interfaz

---
+++ Components/Layout/Modulos/Herramientas.razor.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace Terra.Components.Layout.Modulos
+{
+    public partial class Herramientas
+    {
+        [Inject]
+        private NavigationManager _navigationManager { get; set; }
+
+        private async Task Onclick()
+        {
+            _navigationManager.NavigateTo("/Herramientas");
+        }
+    }
+}
